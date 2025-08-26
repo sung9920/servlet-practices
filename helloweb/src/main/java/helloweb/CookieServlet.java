2625d78b9e7f927a85cdb833d343309b43ea0538
sung9920 <sungcheol9920@gmail.com>
2025-08-26 10:23:44 +0900

    cookie test

---
+++ helloweb/src/main/java/helloweb/CookieServlet.java
@@ -0,0 +1,45 @@
+package helloweb;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class CookieServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int visitCount = 0;
+		// 쿠키 읽기
+		Cookie[] cookies = request.getCookies();
+		if(cookies != null && cookies.length > 0) {
+			for(Cookie cookie : cookies) {
+				if("visitCount".equals(cookie.getName())) {
+					visitCount = Integer.parseInt(cookie.getValue());
+				}
+			}
+		}
+		
+		visitCount++;
+				
+		// 쿠키 쓰기
+		Cookie cookie = new Cookie("visitCount", String.valueOf(visitCount));
+		cookie.setPath(request.getContextPath());
+		cookie.setMaxAge(24*60*60);
+		response.addCookie(cookie);
+		
+		// 출력
+		response.setContentType("text/html; charset=UTF-8");
+		response.getWriter().print("<h1>방문횟수:" + visitCount + "</h1>");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		doGet(request, response);
+	}
+
+}
